d7cf6fdddc3914f11ad258e23e3336f54719e14b
Steven Mah <chunho01@hotmail.com>
2023-09-20 19:45:15 +1000

    Removed unused workflow & js

--- js/carousel.js
+++ js/carousel.js
@@ -30,20 +30,20 @@ nextSlide.addEventListener("click", function () {
   });
 });
 
-// select next slide button
-const prevSlide = document.querySelector(".btn-prev");
+// // select next slide button
+// const prevSlide = document.querySelector(".btn-prev");
 
-// add event listener and navigation functionality
-prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
+// // add event listener and navigation functionality
+// prevSlide.addEventListener("click", function () {
+//   // check if current slide is the first and reset current slide to last
+//   if (curSlide === 0) {
+//     curSlide = maxSlide;
+//   } else {
+//     curSlide--;
+//   }
 
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
+//   //   move slide by 100%
+//   slides.forEach((slide, indx) => {
+//     slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+//   });
+// });
